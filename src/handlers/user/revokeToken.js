6b41e91cc51a7a27561e450f3906ebf2d5290aa2
vasyok <vasiliy.voedilo@thinkmobiles.com>
2017-11-10 14:59:16 +0200

    revoke token

--- src/handlers/user/revokeToken.js
+++ src/handlers/user/revokeToken.js
@@ -1,8 +1,6 @@
 const ClientModel = require('../../models/client');
 const TokenModel = require('../../models/token');
 const generateError = require('../../utils/errorGenerator');
-const {genAccessToken} = require('../../utils/encryptionHelper');
-const {security: {expiresIn: timeToAlive}} = require('../../config');
 
 async function refreshToken(req, res, next) {
     const {client_id: clientId, client_secret: clientSecret, token: refreshToken,} = req.body;
@@ -26,7 +24,18 @@ async function refreshToken(req, res, next) {
             return next(generateError('Token with such refresh token doesn\'t exist'));
         }
 
-         /* TODO There are 3 cases if the access_token is invalid:  1)We refresh and revoke just refreshed token    2)We send message that client must refresh this token   3)We only revoke it*/
+    } catch (e) {
+        return next(e);
+    }
+
+    try {
+        await TokenModel.findOneAndDelete({
+            refreshToken,
+        });
+
+        res.status(200).send({
+            refresh_token: refreshToken,
+        });
     } catch (e) {
         return next(e);
     }
