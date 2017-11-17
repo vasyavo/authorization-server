43a64a60fef60adc9b7f298130f8a97787290f1f
oleksijfomin <oleksij.fomin@gmail.com>
2017-11-17 18:40:39 +0200

    feat(api): Add "/v1/change_password".

---
+++ src/routes/v1/change_password/post.js
@@ -0,0 +1,70 @@
+const UserConnection = require('../../../models/user');
+const ClientConnection = require('../../../models/client');
+const TokenConnection = require('../../../models/token');
+const logger = require('./../../../utils/logger');
+const generateError = require('../../../utils/errorGenerator');
+const {
+    encryptPassword,
+} = require('../../../utils/encryptionHelper');
+const ttl = require('../../../config').security.expiresIn;
+
+async function signIn(req, res, next) {
+    const UserCollection = await UserConnection;
+    const ClientCollection = await ClientConnection;
+    const TokenCollection = await TokenConnection;
+
+    const {
+        client_id: clientId,
+        password,
+        email,
+    } = req.body;
+
+    try {
+        const credentials = {
+            clientId,
+        };
+
+        const client = await ClientCollection.findOne(credentials);
+
+        if (!client) {
+            logger.error('Try to access using application credentials:', credentials);
+
+            return next(generateError('You can\'t sign in through your application'));
+        }
+    } catch (error) {
+        return next(error);
+    }
+
+    let userId;
+
+    try {
+        const result = await UserCollection.findOneAndUpdate({
+            email,
+        }, {
+            $set: {
+                password: encryptPassword(password),
+            },
+        }, {
+            projection: {
+                _id: 1,
+            },
+            returnOriginal: false,
+        });
+
+        const user = result.value;
+
+        if (!user) {
+            return next(generateError('User with such credentials doesn\'t exist'));
+        }
+
+        userId = user._id;
+    } catch (error) {
+        return next(error);
+    }
+
+    res.status(200).send({
+        user_id: userId,
+    });
+}
+
+module.exports = signIn;
